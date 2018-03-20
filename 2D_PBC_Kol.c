c7b54d2631bf0fd0b94e54840f5cece41a87b963
Hemaditya Malla <hemadityamalla@gmail.com>
2018-03-20 11:35:58 +0100

    Added a few lines

---
+++ 2D_PBC_Kol.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+#include <malloc.h>
+
+
+typedef struct {
+	double p[9];
+} populationFn;
+
+typedef struct{
+	double vx,vy;
+} velocity;
